f9dc67f37690256126b8d075f1c06c789cd7f74b
Florencia Roldos <110829678+floroldos@users.noreply.github.com>
2024-06-11 12:15:01 -0300

    detalles

--- src/app/teams/teams.component.ts
+++ src/app/teams/teams.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-teams',
   standalone: true,
-  imports: [],
+  imports: [
+    MenubarModule,
+    NavbarComponent
+  ],
   templateUrl: './teams.component.html',
   styleUrl: './teams.component.scss'
 })
